bd89021f663e3a90f82b2f9d6b0a2fe17c11a592
Judith Mateu <judith.mateu@estudiants.urv.cat>
2024-12-10 19:07:54 +0100

    A PRIORI TOTS ELS BOTS OKEY

--- AdventuresInMinecraft-PC-master/MyAdventures/Bots/OracleBot.py
+++ AdventuresInMinecraft-PC-master/MyAdventures/Bots/OracleBot.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
  # Import necessary modules
@@ -13,3 +14,33 @@ mc = minecraft.Minecraft.create()
 mc.postToChat("Hello Minecraft World")
 pos = mc.player.getTilePos()
 mc.setBlock(pos.x+3, pos.y, pos.z, block.STONE.id)
+
+mc.postToChat("OracleBot està actiu! Pregunteu-me coses sobre Minecraft.")
+
+# Preguntes i respostes predefinides
+faq = {
+    "Com es fa una taula de crafteig?": "Necessites 4 blocs de fusta processada. Col·loca'ls en un quadrat 2x2 al teu inventari.",
+    "Com domesticar un llop?": "Dóna-li ossos fins que apareguin cors. Ara serà el teu aliat!",
+    "Com es fabrica una espasa?": "Necessites 1 pal i 2 materials (fusta, pedra, ferro, or o diamant). Col·loca'ls en forma vertical a la taula de crafteig.",
+    "Què és el Nether?": "El Nether és una dimensió infernal plena de monstres i recursos únics. Pots accedir-hi mitjançant un portal fet d'obsidiana.",
+    "Com trobo diamants?": "Els diamants es troben a les capes baixes del món, normalment entre la 5 i la 12. Utilitza un pic de ferro o millor per extreure'ls.",
+    "Com es fa un portal al Nether?": "Necessites 10 blocs d'obsidiana i un encenedor. Fes un marc rectangular (4 d'alt, 5 d'ample) i encén-lo.",
+    "Com es crafteja un llit?": "Necessites 3 blocs de llana i 3 blocs de fusta. Col·loca'ls en una fila a la taula de crafteig.",
+    "Com es fa un encenedor?": "Necessites 1 barra de ferro i 1 fragment de sílex. Col·loca'ls diagonalment a la taula de crafteig."
+}
+
+# Bucle principal
+while True:
+    # Recuperar missatges nous del xat
+    chat_messages = mc.events.pollChatPosts()
+
+    for message in chat_messages:
+        preguntes = message.message  
+
+        # Respondre si la pregunta està a la llista
+        if preguntes in faq:
+            response = faq[preguntes]  # Obtenir la resposta adient
+            mc.postToChat(f"OracleBot: {response}")
+        else:
+            mc.postToChat("OracleBot: No sé la resposta a aquesta pregunta")
+
--- AdventuresInMinecraft-PC-master/MyAdventures/Bots/TNTBot.py
+++ AdventuresInMinecraft-PC-master/MyAdventures/Bots/TNTBot.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
  # Import necessary modules
@@ -8,8 +9,25 @@ import mcpi.block as block
 import random
 # Connect to the Minecraft game
 mc = minecraft.Minecraft.create()
+mc.postToChat("TNTBot està actiu! Escriviu 'tnt' al xat per activar-me.")
 
-# Interact with the Minecraft world
-mc.postToChat("Hello Minecraft World")
-pos = mc.player.getTilePos()
-mc.setBlock(pos.x+3, pos.y, pos.z, block.STONE.id)
+
+# Bucle principal
+while True:
+    # Recuperar missatges nous del xat
+    chat_messages = mc.events.pollChatPosts()
+
+    for message in chat_messages:
+        if message.message.lower() == "tnt":
+            # Obtenir la posició del jugador
+            pos = mc.player.getTilePos()
+
+            # Col·locar un bloc de TNT davant del jugador
+            mc.setBlock(pos.x + 2, pos.y, pos.z, block.TNT)
+            mc.postToChat("Col·locant TNT...")
+
+            time.sleep(2)  
+
+            mc.setBlock(pos.x + 2, pos.y+1, pos.z, block.FIRE)
+            mc.postToChat("BOOM!")
+
